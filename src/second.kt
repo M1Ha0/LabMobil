41c99269429803e77f10914746cd99f2b52028af
mikhailymikhail@gmail.com <mikhailymikhail@gmail.com>
2025-09-27 11:49:08 +0200

    1

--- src/second.kt
+++ src/second.kt
@@ -1,12 +1,20 @@
 import javax.swing.text.Position
+import java.util.Random
 
 fun main(){
-    val name = readln()
-    hello(name)
+    var arr1=arrayOfNulls<Int>(10)
+    val sum = {x:Int, y:Int ->  for(i in 0..10)
+        {
+        var a = Random().nextInt(10,100)
+            if(a<=y&&a/10+a%10==x){
+                arr1+=a
+                a=0
+            }
+         println(arr1[i])
+        }
+    }
 }
-fun hello(name:String,age:Int=18,position:String="Student"){
-    println("hello, $name!. I am $position, I am $age years old ")
-}
+
 
 
 
