e69f0b12884f7e6fc1b0a7f5d893a8df2b704738
mikhailymikhail@gmail.com <mikhailymikhail@gmail.com>
2025-10-11 11:51:02 +0200

    1223rtyrty

--- src/second.kt
+++ src/second.kt
@@ -2,17 +2,22 @@ import javax.swing.text.Position
 import java.util.Random
 
 fun main(){
-    var arr1=arrayOfNulls<Int>(10)
-    val sum = {x:Int, y:Int ->  for(i in 0..10)
+    var arr1=emptyArray<Int>()
+    val sum = {x:Int, y:Int ->  while(arr1.size<10)
         {
         var a = Random().nextInt(10,100)
             if(a<=y&&a/10+a%10==x){
                 arr1+=a
                 a=0
             }
-         println(arr1[i])
+         println(arr1[+1])
         }
     }
+    println("Введите N")
+    val x = readLine()!!.toInt()
+    println("Введите K")
+    val y = readLine()!!.toInt()
+
 }
 
 
