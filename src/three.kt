fce9d5dc040876982c19a8aa420a2fd4e4d369be
mikhailymikhail@gmail.com <mikhailymikhail@gmail.com>
2025-10-28 11:57:53 +0200

    ytgfuytfiuhssss

--- src/three.kt
+++ src/three.kt
@@ -1,14 +1,21 @@
-class Line(val left:Double,  val right:Double){
-    val s = right - left
-    fun squer()=println(s*s)
-}
 fun main() {
+    //11.1
     print("Введите левую границу: ")
     val left= readLine()!!.toDouble()
     print("Введите правую границу: ")
     val right = readLine()!!.toDouble()
     val line = Line(left, right)
     line.squer()
+    //11.2
 }
+//11.1
+class Line(val left:Double,  val right:Double){
 
+    val s = right - left
+    fun squer()=println(s*s)
+}
+//11.2
+//class Chek(val x:Double) : Line(left,right){
+//
+//}
 
